60000f416b16bca8fe9fd2e88055b93d581ed899
Gavin Mai <tul33743@temple.edu>
2025-01-14 15:21:12 -0500

    ignore fix

--- Assets/Scripts/OnStart.cs
+++ Assets/Scripts/OnStart.cs
@@ -7,7 +7,10 @@ public class OnStart : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject newCube = GameObject.CreatePrimitive(PrimitiveType.Cube); 
+        newCube.transform.position = new Vector3(0, 1, -10); // Set cube position
+        newCube.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f); // Adjust cube size
+
     }
 
     // Update is called once per frame
